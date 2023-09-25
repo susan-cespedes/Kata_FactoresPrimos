f8a6528af35cf0b0978fe33e953505dbd6fb2bd3
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-09-25 11:04:20 -0400

    Deberia retornar una lista para [3,3]

--- src/numerosPrimos.js
+++ src/numerosPrimos.js
@@ -1,17 +1,19 @@
 class primos{
   factorprimo(n){
     let resulprimos = [];
-    if (n>1)
+    let cantidad =2;
+    while (n>1)
     {
-      while (n%2 === 0){
-        resulprimos.push(2);
-        n/=2;
+      while (n%cantidad === 0){
+        resulprimos.push(cantidad);
+        n/=cantidad;
       }
+        cantidad ++;
+    }
       if (n>1)
       {
         resulprimos.push(n);
       }
-    }
     return resulprimos;
   }
 }
